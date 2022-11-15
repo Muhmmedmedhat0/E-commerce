f3d352dbc88f2d56bbb52f750b1254004f7b41b2
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-11-15 22:48:13 +0200

    add head and meta tags

--- FrontEnd/pages/_app.js
+++ FrontEnd/pages/_app.js
@@ -1,15 +1,33 @@
-import '../styles/Global/globals.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import React from "react";
+import Head from "next/head";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '../app/store';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../app/store";
+import "../styles/Global/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <PersistGate loding={null} persistor={persistor}>
+        <Head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="description" content="e-commerce" />
+          <meta name="keywords" content="e-commerce" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          />
+          <title>E-commerce</title>
+        </Head>
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
