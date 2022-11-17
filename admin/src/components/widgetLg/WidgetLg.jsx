1eeb0529442e18b5c9a73b82603d4dc53bcba093
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-11-17 20:52:38 +0200

    fixing big widget

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -8,19 +8,19 @@ export default function WidgetLg() {
   const TOKEN = currentUser?.token;
 
   const [orders, setOrders] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getOrders = async () => {
       try {
         const response = await fetch(`http://localhost:8080/api/orders/`, {
-          method: 'GET',
-          headers: { Authorization: `Bearer ${TOKEN}` },
+          method: 'GET', headers: { Authorization: `Bearer ${TOKEN}` },
         });
         const data = await response.json();
         setOrders(data.orders);
       } catch (error) {
         if (error) {
-          // setErrorFound(error);
+          setError(error);
           console.log(error);
         }
       }
@@ -28,28 +28,29 @@ export default function WidgetLg() {
     getOrders();
   }, [TOKEN]);
   const Button = ({ type }) => {
-    return <button className={'widgetLgButton ' + type}>{type}</button>;
+    return <button className={'widget-lg__button ' + type}>{type}</button>;
   };
   return (
-    <div className="widgetLg">
-      <h3 className="widgetLgTitle">Latest transactions</h3>
-      <table className="widgetLgTable">
-        <thead className="widgetLgTr">
+    <div className="widget-lg">
+      <h3 className="widget-lg__title">Latest transactions</h3>
+      <table className="widget-lg__table">
+        <thead className="widget-lg__tr">
           <tr>
-            <th className="widgetLgTh">Customer</th>
-            <th className="widgetLgTh">Date</th>
-            <th className="widgetLgTh">Amount</th>
-            <th className="widgetLgTh">Status</th>
+            <th className="widget-lg__th">Customer</th>
+            <th className="widget-lg__th">Date</th>
+            <th className="widget-lg__th">Amount</th>
+            <th className="widget-lg__th">Status</th>
           </tr>
         </thead>
         <tbody>
-          {orders?.slice(0, 6)?.map((order) => {
+          { orders.length === 0 ? <h2 style={{ color:"red" }}> No Orders Yet </h2> :
+            orders?.slice(0, 6)?.map((order) => {
             return (
-              <tr className="widgetLgTr" key={order._id}>
-                <td className="widgetLgUser"><span className="widgetLgName">{order.userId}</span></td>
-                <td className="widgetLgDate">{/* <TimeAgo datetime={`${order.createdAt}`} /> */}</td>
-                <td className="widgetLgAmount">${order.amount}</td>
-                <td className="widgetLgStatus"><Button type={order.status} /></td>
+              <tr className="widget-lg-tr" key={order._id}>
+                <td className="widget-lg__user"><span className="widget-lg__name">{order.userId}</span></td>
+                <td className="widget-lg__date">{/* <TimeAgo datetime={`${order.createdAt}`} /> */}</td>
+                <td className="widget-lg-amount">${order.amount}</td>
+                <td className="widget-lg__status"><Button type={order.status} /></td>
               </tr>
             );
           })}
