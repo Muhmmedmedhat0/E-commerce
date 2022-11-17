b4d0b7ea6258009aba6b14eac0d2627b638dee68
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-11-17 21:39:30 +0200

    handling fetuared icon

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,6 +1,6 @@
-import './featuredInfo.css';
 import { ArrowDownward, ArrowUpward } from '@material-ui/icons';
 import { useState, useEffect } from 'react';
+import './featuredInfo.css';
 
 export default function FeaturedInfo() {
 const user = JSON.parse(sessionStorage?.getItem('persist:user'))?.userInfo;
@@ -13,18 +13,13 @@ const currentUser = user && JSON.parse(user);
   useEffect(() => {
     const getIncome = async () => {
       try {
-        const response = await fetch(
-          'http://localhost:8080/api/orders/income',
-          {
-            method: 'GET',
-            headers: { Authorization: `Bearer ${TOKEN}` },
+        const response = await fetch('http://localhost:8080/api/orders/income',{
+            method: 'GET', headers: { Authorization: `Bearer ${TOKEN}` }
           }
         );
         const data = await response.json();
         setIncome(data);
-        setPerc(
-          (response.data[1]?.total * 100) / response.data[0]?.total - 100
-        );
+        setPerc((response.data[1]?.total * 100) / response.data[0]?.total - 100);
       } catch (error) {
         console.log(error);
       }
@@ -33,40 +28,31 @@ const currentUser = user && JSON.parse(user);
   }, [TOKEN]);
   return (
     <div className="featured">
-      <div className="featuredItem">
-        <span className="featuredTitle">Revanue</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">${income[1]?.total || 0} </span>
-          <span className="featuredMoneyRate">
-            % {Math.floor(perc)}
-            {perc <= 0 ? (
-              <ArrowDownward className="featuredIcon negative" />
-            ) : (
-              <ArrowUpward className="featuredIcon" />
-            )}
+      <div className="featured-item">
+        <span className="featured-item__title">Revanue</span>
+        <div className="featured-money">
+          <span className="featured-money__count">${income[1]?.total || 0} </span>
+          <span className="featured-money__rate"> % {Math.floor(perc)}
+            {perc <= 0 ? (<ArrowDownward className="featured__icon negative" />) : (<ArrowUpward className="featured__icon" />)}
           </span>
         </div>
-        <span className="featuredSub">Compared to last month</span>
+        <span className="featured__sub">Compared to last month</span>
       </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Sales</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$4,415</span>
-          <span className="featuredMoneyRate">
-            -1.4 <ArrowDownward className="featuredIcon negative" />
-          </span>
+      <div className="featured-item">
+        <span className="featured-item__title">Sales</span>
+        <div className="featured-money">
+          <span className="featured-money__count">$4,415</span>
+          <span className="featured-money__rate">-1.4 <ArrowDownward className="featured__icon negative" /></span>
         </div>
-        <span className="featuredSub">Compared to last month</span>
+        <span className="featured__sub">Compared to last month</span>
       </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Cost</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,225</span>
-          <span className="featuredMoneyRate">
-            +2.4 <ArrowUpward className="featuredIcon" />
-          </span>
+      <div className="featured-item">
+        <span className="featured-item__title">Cost</span>
+        <div className="featured-money">
+          <span className="featured-money__count">$2,225</span>
+          <span className="featured-money__rate">+2.4 <ArrowUpward className="featured__icon" /></span>
         </div>
-        <span className="featuredSub">Compared to last month</span>
+        <span className="featured__sub">Compared to last month</span>
       </div>
     </div>
   );
